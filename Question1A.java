97c1599d1b69de1c5a1cd50a56bb5b8a85c03dd2
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2018-11-22 00:07:30 +0800

    Update Question1A.java
    i build the random thing

--- Question1A.java
+++ Question1A.java
@@ -1,10 +1,15 @@
 
 package GroupAssignment;
 
+import java.lang.Math;
 import javax.swing.JOptionPane;
+
 public class Question1A {
     public static void main(String[] args) {
-        
-       }
+        String input = JOptionPane.showInputDialog(null,"Enter your 3-digit lottery number : ","Lottery",JOptionPane.QUESTION_MESSAGE);
+        int lottery = 0;
+        lottery += (Math.random()*1000);//To calculate random lottery number
+        System.out.println(lottery);
+       }//end of the main
     
 }
