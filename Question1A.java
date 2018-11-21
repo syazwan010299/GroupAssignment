ba0a9d537d4ea9324c891b2193d72e43274503a7
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2018-11-22 01:08:33 +0800

    x siap lagi

--- Question1A.java
+++ Question1A.java
@@ -10,6 +10,30 @@ public class Question1A {
         int lottery = 0;
         lottery += (Math.random()*1000);//To calculate random lottery number
         System.out.println(lottery);
+        
+        int user = Integer.parseInt(input);
+        
+        int [] a = new int [3];//make array to save the elements 
+        int temp = 0;
+        int i = 0;
+            while ( user > 0){
+                temp = (user % 10);
+                for (; i < 3;){
+                    a[i] = temp;
+                   // System.out.println(temp);
+                    i+=1;
+                }
+                user /= 10;
+            }
+            for ( int j = 0;j < a.length;j++){
+                System.out.print(a[j] + " ");
+            }
+
+        
+        
+        
+        
+        
        }//end of the main
     
 }
