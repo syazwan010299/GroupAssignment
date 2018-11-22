caf8b6dd6d986a4156a25e460fffcbc26ba80b3e
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2018-11-22 15:19:36 +0800

    Update Question1A.java
    change something

--- Question1A.java
+++ Question1A.java
@@ -15,19 +15,13 @@ public class Question1A {
         
         int [] a = new int [3];//make array to save the elements 
         int temp = 0;
-        int i = 0;
+        
             while ( user > 0){
                 temp = (user % 10);
-           
-                for (; i < 3;){
-                    a[i] = temp;
-                   // System.out.println(temp);
-                    i+=1;
-                }
+                
+                
+                
                 user /= 10;
-            }
-            for ( int j = 0;j < a.length;j++){
-                System.out.print(a[j] + " ");
             }
 
         
