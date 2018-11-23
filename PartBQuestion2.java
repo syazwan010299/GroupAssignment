1a4593410b3d451489b07357e9d1c2deef916745
farihasrzli <fryhsrzl@gmail.com>
2018-11-23 21:06:16 +0800

    push

--- PartBQuestion2.java
+++ PartBQuestion2.java
@@ -10,6 +10,10 @@ package GroupAssignment;
 import javax.swing.JOptionPane;
 
 public class PartBQuestion2 {
+    int mark;
+    char grad;
+    double pointer;
+    
     public static void main(String[] args) {
         String num = JOptionPane.showInputDialog(null,"How many students in the class?","Exam Result System",JOptionPane.INFORMATION_MESSAGE);
         System.out.println("Number of students : "+num);
@@ -19,8 +23,24 @@ public class PartBQuestion2 {
             String name = JOptionPane.showInputDialog(null,"Student name?","Exam Result System",JOptionPane.INFORMATION_MESSAGE);
             System.out.print("NAME : "+name);
             String idnum = JOptionPane.showInputDialog(null,"Student ID?","Exam Result System",JOptionPane.INFORMATION_MESSAGE);
-            System.out.println("\t\t ID NO : "+idnum);
+            int id = Integer.parseInt(idnum);
+            while ((id>4)&&(id<4)){
+                System.out.println("\t\t ID NO : "+id);
+            }
+            System.out.println("--------------------------------------------------------------------------------------");
+            String course1 = JOptionPane.showInputDialog(null,"");
+            
         }
         
+    }
+    public void Marks(){
+        if ((mark>=80)&&(mark<101)){
+            grad = 'A';
+            pointer = 4.00;
+        }else if ((mark>=75)&&(mark<=79)){
+            grad = 'B';
+            pointer = 3.67;
+            
+        }
     }
 }
