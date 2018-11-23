3d67a0253e4c93695145e94fa4fcd0890b086122
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2018-11-23 11:56:55 +0800

    i add

--- PartAQuestion2.java
+++ PartAQuestion2.java
@@ -1,9 +1,10 @@
 
 package GroupAssignment;
 
-
+import javax.swing.JOptionPane;
 public class PartAQuestion2 {
     public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog(null,"What is your name?","Counter",JOptionPane.QUESTION_MESSAGE);
         
     }
 }
