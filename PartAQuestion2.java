e50bfcb988a5f9c6c9c06c8891f63a1988466c57
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2018-11-23 20:05:34 +0800

    daa

--- PartAQuestion2.java
+++ PartAQuestion2.java
@@ -7,11 +7,15 @@
  */
 package GroupAssignment;
 
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;//import class JOptionPane
 
 public class PartAQuestion2 {
     public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog(null,"What is your name?","Counter",JOptionPane.QUESTION_MESSAGE);
+        
+        char y ='y';
+        while ( y == 'y'){
+        String input = JOptionPane.showInputDialog(null,"What is your name?","Counter",JOptionPane.QUESTION_MESSAGE);//To ask the user to key in the name
+        
         char name;
         char space;
         int length = input.length();
@@ -21,10 +25,9 @@ public class PartAQuestion2 {
         int counter = 0;
         int size = 0;
         int counter1 = 0;
+        //To check if the character more than 3 character
         if(input.length()>3){
-            System.out.println("Total characters = "+input.length());
-            
-            
+            //To calculate the number of character
             while ( c != 0){
               space = input.charAt(counter1);
              if ( space != ' '){
@@ -33,8 +36,7 @@ public class PartAQuestion2 {
              c--;
              counter1++;
             }
-            
-
+            //To calculate 
             while ( length != 0){
                 name = input.charAt(counter);
                 if ( (name == 'a')||(name == 'e')||(name == 'i')||(name == 'o')||(name == 'u')||(name == 'A')||(name == 'E')||(name == 'I')||(name == 'O')||(name == 'U')){
@@ -44,12 +46,14 @@ public class PartAQuestion2 {
                 }
                 length--;
                 counter++;
-            }
-            
+            }//end of the while loop
+            JOptionPane.showMessageDialog(null,"Name = "+input+"\nTotal characters = "+size+"\nVowels = "+add+"\nConsonants = "+minus,"Counter",JOptionPane.INFORMATION_MESSAGE);   
         }else{
             JOptionPane.showMessageDialog(null,"Try Again","Counter",JOptionPane.INFORMATION_MESSAGE);
         }
-        JOptionPane.showMessageDialog(null,"Name = "+input+"\nTotal characters = "+size+"\nVowels = "+add+"\nConsonants = "+minus,"Counter",JOptionPane.INFORMATION_MESSAGE);
-    }
+        String check = JOptionPane.showInputDialog(null,"Do you want to try again?\n y-YES \t n-NO","Lottery",JOptionPane.QUESTION_MESSAGE);//ask the user to repeat or not 
+        y = check.charAt(0);
+        }//end of the while
+    }      
    
 }//end of the class
