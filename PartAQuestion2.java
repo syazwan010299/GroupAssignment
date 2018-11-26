647befde7ba7a77304786d1a24e01606cc7ddc18
farihasrzli <fryhsrzl@gmail.com>
2018-11-27 00:07:20 +0800

    Update PartAQuestion2.java

--- PartAQuestion2.java
+++ PartAQuestion2.java
@@ -12,31 +12,31 @@ import javax.swing.JOptionPane;//import class JOptionPane
 public class PartAQuestion2 {
     public static void main(String[] args) {
         
-        char y ='y';//act as a variable to show option for the user to repeat the process or not
+        char y ='y';//declare variable for repeat option
         while ( y == 'y'){
-        String input = JOptionPane.showInputDialog(null,"What is your name?","Counter",JOptionPane.QUESTION_MESSAGE);//To ask the user to key in the name
+        String input = JOptionPane.showInputDialog(null,"What is your name?","Alphabets Counter",JOptionPane.QUESTION_MESSAGE);//To ask the user to key in the name
         
-        char name;//to assign the every single word in the user input
-        char space;//to assign the every single word in the user input
+        char name;//to assign every single word in the user input
+        char space;//to assign every single word in the user input
         int length = input.length();//assign the length of input into length variable
         int c = length;//assign the length of input into c variable
-        int add = 0;//act as a variable for the number of vowel
-        int minus = 0;//act as a variable for the number of consonant
-        int counter = 0;//act as a counter to acess every single word
+        int add = 0;//variable for the number of vowel
+        int minus = 0;//variable for the number of consonant
+        int counter = 0;//declare counter to acess every single word
         int size = 0;//to calculate the number of character
         int counter1 = 0;
-        //To check if the character more than 3 character
+        //To check if the character more than 3 characters
         if(input.length()>3){
             //To calculate the number of character
             while ( c != 0){
               space = input.charAt(counter1);
-             if ( space != ' '){
+             if ( space != ' '){//counter is exclude space character
                  ++size;
              }  
              c--;
              counter1++;
             }//end of the while loop
-            //To calculate the vowel and the consonant
+            //To calculate the vowel and the consonant characters
             while ( length != 0){
                 name = input.charAt(counter);
                 if ( (name == 'a')||(name == 'e')||(name == 'i')||(name == 'o')||(name == 'u')||(name == 'A')||(name == 'E')||(name == 'I')||(name == 'O')||(name == 'U')){
@@ -47,7 +47,7 @@ public class PartAQuestion2 {
                 length--;
                 counter++;
             }//end of the while loop
-            JOptionPane.showMessageDialog(null,"Name = "+input+"\nTotal characters = "+size+"\nVowels = "+add+"\nConsonants = "+minus,"Counter",JOptionPane.INFORMATION_MESSAGE);   
+            JOptionPane.showMessageDialog(null,"Name = "+input+"\nTotal characters = "+size+"\nVowels = "+add+"\nConsonants = "+minus,"Alphabets Counter",JOptionPane.INFORMATION_MESSAGE);   
         }else{
             JOptionPane.showMessageDialog(null,"Try Again","Counter",JOptionPane.INFORMATION_MESSAGE);
         }//end of the if else
