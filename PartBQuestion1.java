80a1f6719fe844579ff81678f65fe06759f6e034
farihasrzli <fryhsrzl@gmail.com>
2018-11-27 00:27:57 +0800

    Update PartBQuestion1.java

--- PartBQuestion1.java
+++ PartBQuestion1.java
@@ -38,52 +38,52 @@ public class PartBQuestion1 {
             int product = Integer.parseInt(purchase);
             //Loop as long the product is not 0
             while ( product != 0){
-            String code = JOptionPane.showInputDialog(null,"What is the product code?","Transaction System",JOptionPane.QUESTION_MESSAGE);//ask the user what is the product code for  every item
+            String code = JOptionPane.showInputDialog(null,"What is the product code?","Transaction System",JOptionPane.QUESTION_MESSAGE);//ask the user what is the product code for every item
             int id = Integer.parseInt(code);
             if ( id == 1){//if code item is 1
-             String a1 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a1 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity1 = Integer.parseInt(a1);
-             total1 = (float)(quantity1 * 34.55);//calculate the total price of item 1 times with the quantity of item 1
+             total1 = (float)(quantity1 * 34.55);//calculate the total price of item 1 multiple with the quantity of item 1
              //if the total price is more than RM 200 he/she will get the rebate for the item
              if ( total1 > 200.00){
                  discount = rebate.kira(discount);//calculate the rebate 
              }
-             System.out.println("1\t\tSHOE\t\tRM34.55\t\t"+quantity1+"\t\t\t"+"RM"+total1);//display the code item 1 
+             System.out.println("1\t\tSHOE\t\tRM34.55\t\t"+quantity1+"\t\t\t"+"RM "+total1);//display the code item 1 
             }else if ( id == 2){//if code item is 2
-             String a2 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a2 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity2 = Integer.parseInt(a2);
-             total2 = (float)(quantity2 * 44.29);//calculate the total price of item 2 times with the quantity of item 2
-             System.out.println("2\t\tSHIRT\t\tRM44.29\t\t"+quantity2+"\t\t\t"+"RM"+total2);//display the code item 2 
+             total2 = (float)(quantity2 * 44.29);//calculate the total price of item 2 multiple with the quantity of item 2
+             System.out.println("2\t\tSHIRT\t\tRM44.29\t\t"+quantity2+"\t\t\t"+"RM "+total2);//display the code item 2 
             }else if ( id == 3){//if code item is 3
-             String a3 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a3 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity3 = Integer.parseInt(a3);
-             total3 = (float)(quantity3 * 64.35);//calculate the total price of item 3 with the quantity of item 3
+             total3 = (float)(quantity3 * 64.35);//calculate the total price of item 3 multiple with the quantity of item 3
              //if the total price is more than RM 200 he/she will get the rebate for the item
              if ( total3 > 200.00){
                  discount1 = rebate.kira(discount1);//calculate the rebate
              }
-             System.out.println("3\t\tPANTS\t\tRM64.35\t\t"+quantity3+"\t\t\t"+"RM"+total3);//display the code item 3 
+             System.out.println("3\t\tPANTS\t\tRM64.35\t\t"+quantity3+"\t\t\t"+"RM "+total3);//display the code item 3 
             }else if ( id == 4){//if code item is 4
-             String a4 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a4 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity4 = Integer.parseInt(a4);
-             total4 = (float)(quantity4 * 84.20);//calculate the total price of item 4 with the quantity of item 4
+             total4 = (float)(quantity4 * 84.20);//calculate the total price of item 4 multiple with the quantity of item 4
              //if the total price is more than RM 200 he/she will get the rebate for the item
              if ( total4 > 200.00){
                  discount2 = rebate.kira(discount2);//calculate the rebate
              }
-             System.out.println("4\t\tJEANS\t\tRM84.20\t\t"+quantity4+"\t\t\t"+"RM"+total4);//display the code item 4  
+             System.out.println("4\t\tJEANS\t\tRM84.20\t\t"+quantity4+"\t\t\t"+"RM "+total4);//display the code item 4  
             }else if ( id == 5){//if code item is 5
-             String a5 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a5 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity5 = Integer.parseInt(a5);
              total5 = (float)(quantity5 * 51.59);//calculate the total price of item 5 with the quantity of item 5
-             System.out.println("5\t\tFAN\t\tRM51.59\t\t"+quantity5+"\t\t\t"+"RM"+total5);//display the code item 5  
+             System.out.println("5\t\tFAN\t\tRM51.59\t\t"+quantity5+"\t\t\t"+"RM "+total5);//display the code item 5  
             }else if ( id == 6){//if code item is 6
-             String a6 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a6 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity6 = Integer.parseInt(a6);
              total6 = (float)(quantity6 * 24.45);//calculate the total price of item 6 with the quantity of item 6
-             System.out.println("6\t\tIRON\t\tRM24.45\t\t"+quantity6+"\t\t\t"+"RM"+total6);//display the code item 6    
+             System.out.println("6\t\tIRON\t\tRM24.45\t\t"+quantity6+"\t\t\t"+"RM "+total6);//display the code item 6    
             }else if ( id == 7){//if code item is 7
-             String a7 = JOptionPane.showInputDialog(null,"What is the quantity that you want to purchased","Transaction System",JOptionPane.QUESTION_MESSAGE);
+             String a7 = JOptionPane.showInputDialog(null,"How many you want to purchase?","Transaction System",JOptionPane.QUESTION_MESSAGE);
              quantity7 = Integer.parseInt(a7);
              total7 = (float)(quantity7 * 51.99);//calculate the total price of item 7 with the quantity of item 7
              System.out.println("7\t\tTRAY\t\tRM51.99\t\t"+quantity7+"\t\t\t"+"RM"+total7);//display the code item 7      
