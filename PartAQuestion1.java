07bd1ce9e0d3a102ffb86c747c86f4ebe05c15f7
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2018-11-27 17:30:32 +0800

    daa

--- PartAQuestion1.java
+++ PartAQuestion1.java
@@ -11,25 +11,30 @@ import javax.swing.JOptionPane;//import windows
 
 public class PartAQuestion1 {
     public static void main(String[] args) {
+        int temp1,temp2,temp3;//to save the first, second and third digit from the user input
+        int random1,random2,random3;//to save the first, second and third digit of the random numbers
         JOptionPane.showMessageDialog(null,"Welcome to Lottery Generator!\nTry Your Luck Now and WIN SOME MONEY\nGood Luck :)","Lottery Reward",JOptionPane.INFORMATION_MESSAGE);
         char a = 'y';//variable to loop the process
         while ( a == 'y'){//if the user key in y 
+        int id = 0;//for the counter
+        while ( id == 0){//loop if id equal with 0
         String input = JOptionPane.showInputDialog(null,"Enter your 3-digit lottery number : ","Lottery",JOptionPane.QUESTION_MESSAGE);//ask the user to key in 3 numbers
         int user = Integer.parseInt(input);// Take 3 numbers from user
-        Lot lot = new Lot();
-        int lottery = lot.kira();
         int replace = user;// copy the user input into replace variable
-        int temp1,temp2,temp3;//to save the first, second and third digit from the user input
-        int random1,random2,random3;//to save the first, second and third digit of the random numbers
+        id = user;//copy the value user into the counter
         // To check the amount of digits of user input
         int number = 0;// variable for counter to calculate the number of digits in the user input
         while ( user != 0){
             user /= 10;
             ++number;
         }//end of the while
+        Lot lot = new Lot();
+        int lottery = lot.kira();
+       
         //To check if user input meet the system requirement
         if ((number < 3)||(number > 3)){//if the number of digit less than 3 the system will ask again
             JOptionPane.showMessageDialog(null,"ERROR: Your number is not valid","Lottery",JOptionPane.ERROR_MESSAGE);
+            id -= id;//reset the value id become 0;
         }else{
             temp3 = lot.split(replace);//use the split method to get the third digit of the user input
             replace = lot.delete(replace);//use the delete method to remove the third digit of the user input
@@ -42,7 +47,8 @@ public class PartAQuestion1 {
             random2 = lot.split(lottery);//use the split method to get the second digit of the random number
             lottery = lot.delete(lottery);//use delete method to remove the second digit of the random numbers
             random1 = lot.split(lottery);//use the split method to get the first digit of the random number
-            
+        
+        
             
             //To check the condition to give the reward to the user
             if ((random1==temp1)&&(random2==temp2)&&(random3==temp3)){//if the user input matches the lottery in exact order,the awrd is RM5000
@@ -54,11 +60,13 @@ public class PartAQuestion1 {
             }else{//if any number does not meet lottery
                 JOptionPane.showMessageDialog(null,random1+" | "+random2+" | "+random3+" vs. "+temp1+" | "+temp2+" | "+temp3+"\nHA HA HA You lose.","Lottery Reward",JOptionPane.INFORMATION_MESSAGE);
             }
-        }
+        }//end of the if else
+        }//end of the while
         String check = JOptionPane.showInputDialog(null,"Do you want to try again?\n y-YES \t n-NO","Lottery",JOptionPane.QUESTION_MESSAGE);//ask the user to repeat or not 
         a = check.charAt(0);
-        }//end of the while
-       }//end of the main 
+       }//end of the while
+        
+    }//end of the main
 }//end of the class
 
 class Lot{//new class specialise for to split the numbers
@@ -76,4 +84,5 @@ class Lot{//new class specialise for to split the numbers
         d /= 10;
         return d;
     }
+    
 }//end of Lot class
